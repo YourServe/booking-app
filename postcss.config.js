2a2be12976f820d1312e33b4eb76111dc71ddaba
YourServe <logan@codebreakers.nz>
2025-08-08 23:12:39 +1200

    fix: Correct PostCSS configuration for Tailwind v3

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-// postcss.config.js
-import tailwindPlugin from '@tailwindcss/postcss'
-import autoprefixer    from 'autoprefixer'
-
 export default {
-  plugins: [
-    tailwindPlugin(),    // ← invoke the PostCSS plugin
-    autoprefixer(),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
