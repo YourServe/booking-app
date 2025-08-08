9d6343b499fc76b796f542fa5b0f2293b6a57809
YourServe <logan@codebreakers.nz>
2025-08-08 19:42:31 +1200

    fix: Safelist dynamic Tailwind classes for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-blue-600/80',
+    'border-blue-400',
+    'bg-yellow-500/80',
+    'border-yellow-300',
+    'bg-green-600/80',
+    'border-green-400',
+  ],
   theme: {
     extend: {},
   },
